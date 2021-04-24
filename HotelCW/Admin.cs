92c3a7414097b746819a186cc1e54b83423718ad
fmkpl <70745146+fmkpl@users.noreply.github.com>
2021-04-24 20:49:51 +0300

    2nd commit
    added Admin, Hotel, Country classes

---
+++ HotelCW/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelCW
+{
+    public static class Admin
+    {
+        public static void addHotel(string hotel_name) { }
+        public static void deleteHotel(string hotel_name) { }
+        public static void deleteCountry(string country_name) { }
+        public static void addCountry(string country_name) { }
+    }
+}
